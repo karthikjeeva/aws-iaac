7da15d7ad60b5f528d3537e9d856c8e4277eded2
GitLearner <pj71113@gmail.com>
2023-02-05 12:43:13 +0530

    added lamda apigateway integration

--- apiLamda/lib/api_lamda-stack.ts
+++ apiLamda/lib/api_lamda-stack.ts
@@ -3,29 +3,24 @@ import { Construct } from 'constructs';
 import { Code, Function as LamdaFunction, Runtime } from 'aws-cdk-lib/aws-lambda';
 import { join } from 'path';
 import * as iam from 'aws-cdk-lib/aws-iam';
+import { LambdaIntegration, RestApi } from 'aws-cdk-lib/aws-apigateway';
 
 
 export class ApiLamdaStack extends cdk.Stack {
+  private api = new RestApi(this, 'HelloApi');
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
-   /*  new iam.Role.fr(this, 'cdk-hnb659fds-cfn-exec-role-647327271376-ap-south-1', {
-      assumedBy: new iam.ServicePrincipal('cloudformation.amazonaws.com'),
-      managedPolicies: [
-        iam.ManagedPolicy.fromAwsManagedPolicyName(
-          'AWSLambda_FullAccess',
-        ),
-      ],
-    }); */
-
-    iam.Role.fromRoleArn(this,'AWSLambda_FullAccess', 'arn:aws:iam::647327271376:instance-profile/cdk-hnb659fds-cfn-exec-role-647327271376-ap-south-1');
-    
 
     const helloLamda = new LamdaFunction(this, 'helloLamda', {
       runtime: Runtime.NODEJS_16_X,
       code: Code.fromAsset(join(__dirname, '..', 'services', 'hello')), 
       handler: 'hello.main'
     })
+
+    //api gateway lamda integration
+    const helloLamdaIntegration = new LambdaIntegration(helloLamda);
+    const helloLamdaResouce = this.api.root.addResource('hello');
+    helloLamdaResouce.addMethod('GET', helloLamdaIntegration);
 
   
   }
